dfe40c629504d3c8d51ac9978a558779930a09fe
LinarKhil <zzlinarzz@gmail.com>
2018-07-25 18:48:36 +0500

    User displaying

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
-var cookieParser = require('cookie-parser')
+var user_name = ''
+var user_names = new Map();
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser())
-var user_list = []
-var user_name = ''
+
 app.get('/', function(req, res) 
 	{
 		//res.render('index')
@@ -16,34 +18,23 @@ app.get('/', function(req, res)
 	});
 io.on('connection', function(socket)
 	{
-		//console.log('a user connected')
-		//
-		
+		user_names[socket.id] = user_name;
 		socket.on('disconnect', function()
 			{
 				console.log('user disconnected')
 				//console.log(user_list[0]);
 			});
-
-		socket.on('disconnecting', function()
-			{
-			});
 		socket.on('chat message', function(msg)
 		{
-			io.emit('chat message', user_name + ': ' + msg);
+			io.emit('chat message', user_names[socket.id] + ': ' + msg);
 		});
-
-
 	});
-
-
 app.post('/', function(req, res) 
 	{
 		console.log(req.body.firstname + ' connected to chat');
 		user_name = req.body.firstname;
 		res.sendFile(__dirname + '/views/chat.html')
-	})
-
+	});
 http.listen(3000, function () {
   console.log('Server is running')
 });
